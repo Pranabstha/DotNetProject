84959e3191c2369b8f51ee3495dda40850042b86
Pranab Shrestha <pranabstha780@gmail.com>
2024-05-08 13:59:42 +0545

    Add repository for User Authentication

--- Interfaces/IUserAuthenticationRepository.cs
+++ Interfaces/IUserAuthenticationRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using BislariumCW.Dtos;
 using Microsoft.AspNetCore.Identity;
 
 namespace BislariumCW.Interfaces
---
+++ Mappings/UserMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BislariumCW.Dtos;
+using BislariumCW.Models;
+
+namespace BislariumCW.Mappings
+{
+    public class UserMappingProfile : Profile
+    {
+        public UserMappingProfile()
+        {
+            CreateMap<ApplicationUser, UserRegistrationDto>().ReverseMap();
+        }
+    }
+}
